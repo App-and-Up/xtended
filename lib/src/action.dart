7a0c69ec89545712442c6c2200123f1c168910c1
Daniele Cambi <43640732+dancamdev@users.noreply.github.com>
2023-06-30 12:02:38 +0200

    adds doc comments

--- lib/src/action.dart
+++ lib/src/action.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/widgets.dart';
 
+/// A set of actions that can be performed on a widget.
+/// 
+/// This enum is used by the [action] extension on [Widget].
 enum Action {
   tap,
   doubleTap,
   longPress;
 
+  /// Converts the action to a [GestureDetector] with the specified action.
   GestureDetector toGestureDetector(VoidCallback? action, Widget child) {
     return switch (this) {
       Action.tap => GestureDetector(onTap: action, child: child),
@@ -15,6 +19,26 @@ enum Action {
 }
 
 extension ActionXtended on Widget {
+  /// An extension on [Widget] that adds the ability to perform actions on it.
+  ///
+  /// Example:
+  ///
+  /// Prints 'Hello World' when the [Text] widget is tapped.
+  /// ```dart
+  /// Text('Hello World').action(() => print('Hello World'))
+  /// ```
+  ///
+  /// Prints 'Hello World' when the [Text] widget is double tapped.
+  /// ```dart
+  /// Text('Hello World').action(() => print('Hello World'), Action.doubleTap)
+  /// ```
+  ///
+  /// Prints 'Hello World' when the [Text] widget is long pressed.
+  /// ```dart
+  /// Text('Hello World').action(() => print('Hello World'), Action.longPress)
+  /// ```
+  ///
+  /// You can also concatenate them, to perform different actions on the same widget.
   Widget action(VoidCallback action, [Action actionType = Action.tap]) {
     return actionType.toGestureDetector(action, this);
   }
